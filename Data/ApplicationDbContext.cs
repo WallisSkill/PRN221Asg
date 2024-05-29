d125f9ee4a86c2abeaaf83a6ce25af540725fff4
anhnbhhe172228 <anhnbhhe172228@fpt.edu.vn>
2024-05-29 18:45:12 +0700

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PRN221_Assignment.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
