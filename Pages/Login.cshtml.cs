ed6b77c9028281b8b62ed2edbfb8237eecf34202
WallisSkill <hoanganh23072@gmail.com>
2024-06-02 15:17:06 +0700

    Add refer user example

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -29,7 +29,9 @@ namespace PRN221_Assignment.Pages
                 List<Claim> listClaim = new List<Claim>()
             {
                 new Claim(ClaimTypes.NameIdentifier, userLogin.Username),
-                new Claim(ClaimTypes.Name, userLogin.Username)
+                new Claim(ClaimTypes.Name, userLogin.Username),
+                new Claim(ClaimTypes.Email, userLogin.Email),
+                new Claim("profile_picture", userLogin.ProfilePhotoUrl)
             };
                 ClaimsIdentity ci = new ClaimsIdentity(listClaim, Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults.AuthenticationScheme);
                 ClaimsPrincipal cp = new ClaimsPrincipal(ci);
