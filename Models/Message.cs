97b6aa2a0582d16f7aa8b7f8310aca22f722a822
Hung <hungnvhe170570@fpt.edu.vn>
2024-06-08 21:16:51 +0700

    add content column to table message

--- Models/Message.cs
+++ Models/Message.cs
@@ -8,6 +8,7 @@ namespace PRN221_Assignment.Models
         public int MessageId { get; set; }
         public int SenderId { get; set; }
         public int ReceiverId { get; set; }
+        public string? Content { get; set; }
         public int TypeId { get; set; }
         public DateTime? SendAt { get; set; }
         public bool? IsRead { get; set; }
