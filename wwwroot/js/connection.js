b2f891e3f07998a72fc5b760b1c8014d7b2aa821
WallisSkill <hoanganh23072@gmail.com>
2024-06-17 12:14:03 +0700

    fix layout connection

---
+++ wwwroot/js/connection.js
@@ -0,0 +1,5 @@
+﻿const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/hub")
+    .withAutomaticReconnect([0, 0, 10000])
+    .build();
+connection.start().catch(err => console.error(err.toString()));
