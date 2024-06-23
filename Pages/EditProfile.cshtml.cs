c6e1ae829558f1b87e1999e477c562af7d4f842e
LongNoobb <124030674+LongNoobb@users.noreply.github.com>
2024-06-24 00:09:04 +0700

    add edit profile page

---
+++ Pages/EditProfile.cshtml.cs
@@ -0,0 +1,16 @@
+using Lombok.NET;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PRN221_Assignment.Services.Interface;
+
+namespace PRN221_Assignment.Pages
+{
+    public class EditProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+            
+    }
+}
